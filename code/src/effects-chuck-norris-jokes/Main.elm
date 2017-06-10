09b1ace6599507a05ecf0502770c28da0f1206c6
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-06-10 17:23:31 +0200

    We're GETting somewhere

--- code/src/effects-chuck-norris-jokes/Main.elm
+++ code/src/effects-chuck-norris-jokes/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Http exposing (..)
 
 
 type alias Model =
@@ -12,15 +13,23 @@ initModel =
     "Finding a joke..."
 
 
+init : ( Model, Cmd Msg )
+init =
+    ( initModel, randomJoke )
+
+
 type Msg
-    = Joke String
+    = Joke (Result Error String)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Joke joke ->
-            joke
+        Joke (Ok joke) ->
+            ( joke, Cmd.none )
+
+        Joke (Err error) ->
+            ( error |> toString, Cmd.none )
 
 
 view : Model -> Html Msg
@@ -30,10 +39,31 @@ view model =
         ]
 
 
+randomJoke : Cmd Msg
+randomJoke =
+    let
+        url =
+            "https://api.icndb.com/jokes/random"
+
+        request =
+            Http.getString url
+
+        cmd =
+            Http.send Joke request
+    in
+        cmd
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
 main : Program Never Model Msg
 main =
-    Html.beginnerProgram
-        { model = initModel
+    Html.program
+        { init = init
         , view = view
         , update = update
+        , subscriptions = subscriptions
         }
