19ebda7f5ea8feeda4375dec291295f4393f8e4d
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-06-10 12:51:19 +0200

    starting on realistic TEA SPA example, first view done

---
+++ code/src/elm-architecture-tea-demo/PageA.elm
@@ -0,0 +1,42 @@
+module PageA exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+type alias Model =
+    { name : String }
+
+
+initModel : Model
+initModel =
+    { name = "Bob" }
+
+
+type Msg
+    = NameChange String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NameChange new ->
+            { model | name = new }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text <| "Hey " ++ model.name ]
+        , input [ type_ "text", value model.name, onInput NameChange ] []
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = initModel
+        , view = view
+        , update = update
+        }
