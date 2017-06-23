733db3c319850c92d3c29335c570d60dfd2fd2b5
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-06-23 20:38:54 +0200

    example of a failing decode

--- code/src/json/tests/JSON.elm
+++ code/src/json/tests/JSON.elm
@@ -8,6 +8,28 @@ import Expect
 --How to run: <elm-test --watch> in root of project
 
 
+json : String
+json =
+    """
+    {
+      "type": "success",
+      "value":
+      {
+        "id": 541,
+        "joke": "When Chuck Norris break the build, you can't fix it, because there is not a single line of code left.",
+        "categories": ["nerdy"]
+      }
+    }
+    """
+
+
+type alias Joke =
+    { id : Int
+    , joke : String
+    , categories : List String
+    }
+
+
 suite : Test
 suite =
     describe "JSON Decoding"
@@ -16,18 +38,38 @@ suite =
                 let
                     jokeDecoder =
                         at [ "value", "joke" ] string
-
-                    json =
-                        """
-                        {
-                        "type": "success",
-                        "value": {
-                          "id": 541,
-                          "joke": "When Chuck Norris break the build, you can't fix it, because there is not a single line of code left.",
-                          "categories": ["nerdy"] } }
-                        """
                 in
                     Expect.equal
                         (decodeString jokeDecoder json)
-                        (Result.Ok "When Chuck Norris break the build, you can't fix it, because there is not a single line of code left.")
+                        (Ok "When Chuck Norris break the build, you can't fix it, because there is not a single line of code left.")
+        , test "Returns errors when cannot parse using the Result type" <|
+            \() ->
+                let
+                    wrongDecoder =
+                        at [ "foo" ] int
+
+                    decodeResult =
+                        decodeString wrongDecoder json
+                in
+                    Expect.equal
+                        decodeResult
+                        (Err "Expecting an object with a field named `foo` but instead got: {\"type\":\"success\",\"value\":{\"id\":541,\"joke\":\"When Chuck Norris break the build, you can't fix it, because there is not a single line of code left.\",\"categories\":[\"nerdy\"]}}")
+        , test "Can extract into record types" <|
+            \() ->
+                let
+                    decoder =
+                        map3 Joke
+                            (field "id" int)
+                            (field "joke" string)
+                            (field "categories" (list string))
+                            |> at [ "value" ]
+                in
+                    Expect.equal
+                        (decodeString decoder json)
+                        (Ok
+                            { id = 541
+                            , joke = "When Chuck Norris break the build, you can't fix it, because there is not a single line of code left."
+                            , categories = [ "nerdy" ]
+                            }
+                        )
         ]
