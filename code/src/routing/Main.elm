7766a1749aa71816cf98324022e8ceb2e9e8ec20
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-06-30 18:48:52 +0200

    bare-knuckle routing demo before playing with elm-lang/navigation

---
+++ code/src/routing/Main.elm
@@ -0,0 +1,93 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (..)
+
+
+type Page
+    = LeaderBoard
+    | AddRunner
+    | Login
+    | NotFound
+
+
+type alias Model =
+    { page : Page
+    }
+
+
+initModel : Model
+initModel =
+    { page = LeaderBoard }
+
+
+type Msg
+    = ChangePage Page
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ChangePage page ->
+            ( { model | page = page }, Cmd.none )
+
+
+menu : Model -> Html Msg
+menu model =
+    header []
+        [ a [ onClick (ChangePage LeaderBoard) ]
+            [ text "LeaderBoard" ]
+        , text " | "
+        , a [ onClick (ChangePage AddRunner) ]
+            [ text "Add Runner" ]
+        , text " | "
+        , a [ onClick (ChangePage Login) ]
+            [ text "Login" ]
+        ]
+
+
+viewPage : String -> Html Msg
+viewPage pageDescription =
+    div []
+        [ h3 [] [ text pageDescription ]
+        , p [] [ text <| "TODO: make " ++ pageDescription ]
+        ]
+
+
+view : Model -> Html Msg
+view model =
+    let
+        page =
+            case model.page of
+                LeaderBoard ->
+                    viewPage "LeaderBoard Page"
+
+                AddRunner ->
+                    viewPage "Add Runner Page"
+
+                Login ->
+                    viewPage "Login Page"
+
+                NotFound ->
+                    viewPage "Page Not Found"
+    in
+        div []
+            [ menu model
+            , hr [] []
+            , page
+            ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = ( initModel, Cmd.none )
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
