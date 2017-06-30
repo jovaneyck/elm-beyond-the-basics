ceab1457894fb64e4c946e54b291ad51aaf9907d
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-06-30 18:37:31 +0200

    Added full scala of Elm HTML tests. Nice stuff!

--- code/src/testing/Main.elm
+++ code/src/testing/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
 add : number -> number -> number
@@ -24,13 +25,19 @@ update msg model =
 
 
 type Msg
-    = Clicked
+    = ClickedAButton
+    | ClickedAnotherButton
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ text <| "Hello " ++ model.name ]
+        [ div []
+            [ div [ id "name-tag", class "red blue" ]
+                [ text <| "Hello " ++ model.name ]
+            ]
+        , button [ onClick ClickedAButton ] [ text "Click me" ]
+        ]
 
 
 main : Program Never Model Msg
--- code/src/testing/tests/HtmlTests.elm
+++ code/src/testing/tests/HtmlTests.elm
@@ -4,14 +4,38 @@ import Test exposing (..)
 import Main exposing (..)
 import Test.Html.Query as Query
 import Test.Html.Selector exposing (..)
+import Test.Html.Event as Event
 
 
 suite : Test
 suite =
     describe "HTML testing basics"
-        [ test "Can check whether certain text appears on a page" <|
+        [ test "Can check whether text appears on a page" <|
             \() ->
                 Main.view Main.initModel
                     |> Query.fromHtml
                     |> Query.has [ text "Jo" ]
+        , test "Can select HTML elements" <|
+            \() ->
+                Main.view Main.initModel
+                    |> Query.fromHtml
+                    |> Query.find [ id "name-tag" ]
+                    |> Query.has [ text "Jo" ]
+        , test "Can assert on stuff like css classes" <|
+            \() ->
+                Main.view Main.initModel
+                    |> Query.fromHtml
+                    |> Query.find [ id "name-tag" ]
+                    |> Query.has [ classes [ "red", "blue" ] ]
+        , test "Can simulate user interaction" <|
+            \() ->
+                let
+                    button =
+                        Main.view Main.initModel
+                            |> Query.fromHtml
+                            |> Query.find [ tag "button" ]
+                in
+                    button
+                        |> Event.simulate (Event.click)
+                        |> Event.expect (Main.ClickedAButton)
         ]
